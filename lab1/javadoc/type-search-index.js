dc3ff25e71f5493848f32e9ee3020e21764e09d6
Ilya <kramnik.i2003@gmail.com>
2023-06-02 00:04:07 +0300

    maven added

--- lab1/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FileHandler"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
